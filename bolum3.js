4e0594b7890a9a4fdd383064d609c2edda5ecb7d
DevEmir25 <emircanoz0101@gmail.com>
2022-05-24 13:39:15 +0300

    yenidenbasla degisti

--- bolum3.js
+++ bolum3.js
@@ -412,7 +412,7 @@ function dusmanlariGuncelle() {
 
 function kaybettin() {
     cancelAnimationFrame(animasyonId);
-    window.location.href = "yenidenBaslaEkrani.html"
+    window.location.href = "yenidenBaslaEkrani3.html"
 
 }
 function kazandin() {
